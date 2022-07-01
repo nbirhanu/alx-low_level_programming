321b84de9a2068fa551357daa7f7abda6365c32c
nbirhanu <nbirhanu80@gmail.com>
2022-07-01 14:17:49 -0700

    cool

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,8 @@
-#include "hash_tables.h"
-
 /**
-* hash_djb2 - function that generates a random number through
-* a string given (hash number)
+* hash_djb2 - implementation of the djb2 algorithm
+* @str: string used to generate hash value
 *
-* @str: string
-* Return: hash number
+* Return: hash value
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -14,7 +11,7 @@ int c;
 hash = 5381;
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) + c; /* hash * 33 +c */
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
 return (hash);
 }
